ab430e48edf1c7be38647c146fce3d968b4a4ad2
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-10-28 19:57:59 +0530

    Create BrandRouter.js

---
+++ backend/routes/api/v2/BrandRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+const brandController= require('../../../controllers/v2/BrandController');
+
+router.get('/', brandController.getAllBrands);
+
+module.exports = router;
+
