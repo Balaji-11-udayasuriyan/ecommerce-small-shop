5d379f7f94ac5017d43ced2b03b84027ea17aa34
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-10-30 20:08:31 +0530

    Update BrandController.js
    (res, req) to (req, res) . api issue is fixed

--- backend/controllers/v2/BrandController.js
+++ backend/controllers/v2/BrandController.js
@@ -1,17 +1,13 @@
 const Brand = require('../../models/Brand');
 
-exports.getAllBrands = async (res, req)=>{
-
-    try{
-
-        const brands = await Brand.find();
-
-        res.status(200).json({data: brands});
-
-    }catch(err){
-
-        res.status(500).json({error: err.message});
+exports.getAllBrands = async (req, res) => {
+    try {
         
-    }
+        const brands = await Brand.find();
+        res.status(200).json({ data: brands });
 
-}
+    } catch (error) {
+
+        res.status(500).json({ error: error.message });
+    }
+};
