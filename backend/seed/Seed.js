6effcdf8b47f59680c2cf39e016f3d89cdcde336
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-10-30 21:25:34 +0530

    Update seed.js to include ProductSeeder for product data population

--- backend/seed/Seed.js
+++ backend/seed/Seed.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const seedCategories = require('./CategorySeeder'); // Import the seedCategories function
 const seedBrands = require('./BrandSeeder'); // Import the seedCategories function
 const seedSubCategories= require('./SubCategorySeeder'); // Import the seedCategories function
+const seedProducts= require('./ProductSeeder'); // Import the seedCategories function
 
 const { connectDB, disconnectDB } = require('../config/db'); // Import database connection functions
 
@@ -16,12 +17,15 @@ async function runSeeders() {
 
         console.log('Seeding categories...');
         await seedCategories();  // Seed categories first
+
+        console.log('Seeding subcategories...');
+        await seedSubCategories();  // Seed subcategories first
         
         console.log('Seeding brands...');
         await seedBrands();  // Seed categories first
 
-        console.log('Seeding subcategories...');
-        await seedSubCategories();  // Seed subcategories first
+        console.log('Seeding products...');
+        await seedProducts();  // Seed categories first
 
     } catch (err) {
         console.error('Error during seeding:', err);
