c27d069900c30888bc77042031ef7d8d01a2b7a0
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-11-02 09:23:37 +0530

    Update CartRouter.js

--- backend/routes/api/v2/CartRouter.js
+++ backend/routes/api/v2/CartRouter.js
@@ -2,19 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const {
-    getAllCarts,
-    addItemToCart,
-    updateCartItem,
-    removeCartItem,
-    clearCart
-} = require('../../../controllers/v2/CartController');
+const cartController = require('../../../controllers/v2/CartController');
 
 // Define routes for each cart operation
-router.get('/', getAllCarts);          // Get all items in the cart
-router.post('/', addItemToCart);       // Add an item to the cart
-router.put('/:id', updateCartItem);    // Update quantity of an item in the cart
-router.delete('/:id', removeCartItem); // Remove an item from the cart
-router.delete('/', clearCart); 
+router.get('/', cartController.getAllCarts);          // Get all items in the cart
+router.post('/', cartController.addItemToCart);       // Add an item to the cart
+router.put('/:id',cartController. updateCartItem);    // Update quantity of an item in the cart
+router.delete('/:id', cartController.removeCartItem); // Remove an item from the cart
+router.delete('/', cartController.clearCart); 
 
 module.exports = router;
