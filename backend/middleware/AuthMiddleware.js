15672ae9c2f00c1a8f5e0148f7c3fa2da16451e7
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-11-06 12:00:17 +0530

    Update authMiddleware and correct token extraction logic
    - Ensured proper token format for validation

--- backend/middleware/AuthMiddleware.js
+++ backend/middleware/AuthMiddleware.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const authMiddleware = (req, res, next) => {
 
-    const token = req.header('Authorization')?.replace('Bearer','');
+    const token = req.header('Authorization')?.replace('Bearer', '').trim();;
     if (!token) return res.status(401).json({ message: 'Unauthorized'});
 
     try{
@@ -20,3 +20,22 @@ const authMiddleware = (req, res, next) => {
 };
 
 module.exports = authMiddleware;
+
+
+
+// const jwt = require('jsonwebtoken');
+
+// module.exports = (req, res, next) => {
+
+//     // const token = req.headers['authorization']?.split('')[1];
+//     const token = req.headers['authorization']?.split(' ')[1];  // Corrected split syntax
+//     if (!token) return res.status(403).send('A token is reqired for authentication');
+
+//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+
+//         if (err) return res.status(401).send('Invalid Token');
+//         req.user = decoded;
+//         next();
+//     });
+
+// };
