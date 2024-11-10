acf67d9842c43834831f1a4540b561c2b6568fce
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-11-10 21:19:14 +0530

    Update OrderItem.js

--- backend/models/OrderItem.js
+++ backend/models/OrderItem.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const OrderItemSchema = new mongoose.Schema({
 
-    order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
+    order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order'},
 
     product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
 
     quantity: { type: Number, required: true },
-    
+
     price: { type: Number, required: true }  // Price at the time of order
 
 }, { timestamps: true });
