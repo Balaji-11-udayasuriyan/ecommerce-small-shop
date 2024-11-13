38372d140f94ee66855252371f2a77239aa72254
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-11-13 14:08:15 +0530

    Update and add login route in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppLayout from './components/layout/AppLayout';
 import HomePage from './components/HomePage';
+import Login from './components/auth/Login';
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
             <Routes>
 
                 <Route path ="/" element={<HomePage/>} />
+
+                <Route path ="/login" element={<Login/>} />
 
             </Routes>
 
