f77076c0ca878010870cbe883fa021a2a0b2690d
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-11-17 21:33:21 +0530

    Update and add about route in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import ForgetPassword from './components/auth/ForgetPassword';
 import ResetPassword from './components/auth/ResetPassword';
+import About from './components/AboutPage';
 
 function App() {
 
@@ -31,6 +32,8 @@ function App() {
                 <Route path="/forgetpassword" element={<ForgetPassword/>} />
 
                 <Route path="/resetpassword" element = {<ResetPassword/>} />
+
+                <Route path='/about' element= {<About/>} />
 
             </Routes>
 
