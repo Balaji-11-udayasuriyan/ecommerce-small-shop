1a58e163a54e0a65ce1472e2cfff1ea6ea0e020a
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-11-18 11:41:47 +0530

    integrate PopularProduct component into HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,16 +1,11 @@
 import React from "react";
+import PopularProduct from "./slider/PopularProduct";
 
 const HomePage = () => {
-
   return (
-    <div>
-
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main page of the application.</p>
-      <p>Explore our features and navigate through the app using the links above.</p>
-    </div>
-    
+      <PopularProduct/>
   );
+  
 };
 
 export default HomePage;
