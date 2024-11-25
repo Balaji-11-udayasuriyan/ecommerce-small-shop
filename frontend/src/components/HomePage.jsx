f3857bfe5875dbde8cc339e79a5ec89516f92ce1
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-11-25 09:26:57 +0530

    integrate FeatureCategory component into HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import PopularProduct from "./slider/PopularProduct";
 import DailyBestSells from "./slider/DailyBestSells";
 import DealOfTheDay from "./slider/DealOfTheDay";
+import FeatureCategory from "./slider/FeatureCategory";
 
 const HomePage = () => {
   return (
     <div>
+
+      {/* Feature Category */}
       
+        <FeatureCategory />
+
       {/* Popular Products  */}
       
         <PopularProduct />
