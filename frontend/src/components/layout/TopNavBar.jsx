ddd6587b4e615a743c80ffb6969da4e2c35c9b8e
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-11-25 09:27:03 +0530

    Update TopNavBar.jsx

--- frontend/src/components/layout/TopNavBar.jsx
+++ frontend/src/components/layout/TopNavBar.jsx
@@ -14,27 +14,6 @@ const TopBar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                    </li>
-
-                    {!isAuthenticated ? (
-                        <li className="nav-item">
-                            <a className="nav-link" href="/login">Login / Register</a>
-                        </li>
-                    ) : (
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                {userName} {/* Display the user's name */}
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Cart</a></li>
-                                <li><a className="dropdown-item" href="#">Order</a></li>
-                                <li><a className="dropdown-item" href="#">Profile</a></li>
-                                <li><a className="dropdown-item" href="#" onClick={handleLogout}>Logout</a></li>
-                            </ul>
-                        </li>
-                    )}
                 </ul>
             </div>
         </nav>
